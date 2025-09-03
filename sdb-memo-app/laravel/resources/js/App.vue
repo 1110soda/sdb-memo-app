ff79b561e87ef2d2076458ef850dfa308aed9b84
1110soda <5o7a1110@gmail.com>
2025-09-03 16:19:17 +0900

    feat: ヘッダーの追加（App.vue, Header.vue, app.blade.php, tailwind.config.tsを編集）

--- sdb-memo-app/laravel/resources/js/App.vue
+++ sdb-memo-app/laravel/resources/js/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <router-view />
+    <div>
+        <Header :is-loggedIn="isLoggedIn" :user-name="userName" />
+        <main class="p-4">
+            <router-view />
+        </main>
+    </div>
 </template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Header from './components/Header.vue';
+
+const isLoggedIn = ref(false);
+const userName = ref('');
+
+onMounted(() => {
+//  ユーザーのログイン状態とユーザー名をバックエンドから取得する
+    const auth = document.querySelector('body[data-auth]')?.getAttribute('data-auth');
+    if (auth) {
+        isLoggedIn.value = true;
+        userName.value = '山田太郎'; //ダミーのユーザー名
+    }
+})
+</script>
---
+++ sdb-memo-app/laravel/resources/js/components/Header.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import DocumentSvg from './svgs/DocumentSvg.vue';
+
+const props = defineProps({
+    isLoggedIn: Boolean,
+    userName: String,
+});
+
+const title = computed(() => {
+    return props.isLoggedIn ? `${props.userName}'s memo` : `1110soda's memo app`;
+});
+</script>
+
+<template>
+    <header class="p-4 flex flex-col items-center border-b border-secondary-200">
+        <div class="w-full flex justify-end mb-2">
+            <nav>
+                <ul class="flex space-x-4">
+                    <template v-if="isLoggedIn">
+                        <li><a href="/history" class="text-secondary-500 border-r border-secondary-200 pr-4 hover:text-secondary-900 transition-colors">
+                            履歴
+                        </a></li>
+                        <li><a href="/logout" class="font-bold text-secondary-500 hover:text-secondary-900 transition-colors">
+                            ログアウト
+                        </a></li>
+                    </template>
+                    <template v-else>
+                        <li><a href="/createUser" class="text-secondary-500 border-r border-secondary-200 pr-4 hover:text-secondary-900 transition-colors">
+                            新規登録
+                        </a></li>
+                        <li><a href="/login" class="font-bold text-secondary-500 hover:text-secondary-900 transition-colors">
+                            ログイン
+                        </a></li>
+                    </template>
+                </ul>
+            </nav>
+        </div>
+        <a href="/" class="text-center no-underline">
+            <div class="flex items-center space-x-2">
+                <DocumentSvg class="w-8 h-8 text-accent-800" />
+                <h1 class="text-3xl font-thin text-accent-800">
+                    {{ title }}
+                </h1>
+            </div>
+        </a>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
